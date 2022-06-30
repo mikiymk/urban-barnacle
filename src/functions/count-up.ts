1fdfed673132d93244e1341490b3712840f007d6
ymk <65121726+mikiymk@users.noreply.github.com>
2022-06-30 20:43:44 +0900

    Update count-up.ts

--- src/functions/count-up.ts
+++ src/functions/count-up.ts
@@ -1,3 +1,8 @@
+/**
+ * *provide* number increments
+ * @param start counting first value
+ * @returns new iterator
+ */
 export function countUp(start: number): Generator<number>; 
 export function* countUp(start = 0): Generator<number> {
   while (true) {
