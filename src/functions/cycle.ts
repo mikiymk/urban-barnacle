124e25c5b07c998fe2cf657c4cb7c34899878d3a
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-01 02:42:47 +0900

    Update cycle.ts

--- src/functions/cycle.ts
+++ src/functions/cycle.ts
@@ -1,16 +1,24 @@
-export function* cycle<T, TNext>(iterable: Iterator<T, void, TNext>): Generator<T, never, TNext> {
+export function* cycle<T, TNext>(iterator: Iterator<T, void, TNext>): Generator<T, never, TNext> {
   let cur = iterator.next();
-  const valueCollector = [cur.value];
+  const values = [cur.value];
 
   while (!cur.done) {
-    const next = yield cur.value;
-    cur = iterator.next(next);
-    valueCollector.push(cur.value);
+    try {
+      const next = yield cur.value;
+      cur = iterator.next(next);
+      values.push(cur.value);
+    } catch (error) {
+      iterator.throw?.(error);
+    }
   }
 
   while (true) {
-    for (const value of valueCollector) {
-      yield value;
+    for (const value of values) {
+      try {
+        yield value;
+      } catch (error) {
+        iterator.throw?.(error);
+      }
     }
   }
 }
