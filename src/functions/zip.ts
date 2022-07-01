bd3ac4d77596f0d1303e026eb03957ea09d7ed6c
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-01 09:40:06 +0900

    Update zip.ts

--- src/functions/zip.ts
+++ src/functions/zip.ts
@@ -6,7 +6,22 @@ export function* zip<T, TReturn, TNext>(...iterators: Iterator<T, TReturn, TNext
       const next = yield curs.map((cur) => cur.value);
       curs = iterators.map((iterator) => iterator.next(next));
     } catch (error) {
-      iterators.map((iterator) => iterator.throw?.(error));
+      iterators.forEach((iterator) => iterator.throw?.(error));
+    }
+  }
+
+  return curs.map((cur) => cur.value);
+}
+
+export function* zipLong<T, TReturn, TNext>(...iterators: Iterator<T, TReturn, TNext>[]): Generator<T[], TReturn[], TNext> {
+  let curs = iterators.map((iterator) => iterator.next());
+
+  while (curs.some((cur) => !cur.done)) {
+    try {
+      const next = yield curs.map((cur) => cur.value);
+      curs = iterators.map((iterator) => iterator.next(next));
+    } catch (error) {
+      iterators.forEach((iterator) => iterator.throw?.(error));
     }
   }
 
