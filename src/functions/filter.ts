e12ed8635fb2011bc2c68ea77630b6d71d67db03
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-01 10:07:14 +0900

    Create filter.ts

---
+++ src/functions/filter.ts
@@ -0,0 +1,18 @@
+export function* filter<T, TReturn, TNext>(iterator: Iterator<T, TReturn, TNext>, filterFunction: (value: T) => boolean): Generator<T, TReturn, TNext> {
+  let cur = iterator.next();
+
+  while (!cur.done) {
+    try {
+      if (filterFunction(cur.value)) {
+        const next = yield cur.value;
+        cur = iterator.next(next);
+      } else {
+        cur = iterator.next();
+      }
+    } catch (error) {
+      iterator.throw?.(error);
+    }
+  }
+
+  return cur.value;
+}
