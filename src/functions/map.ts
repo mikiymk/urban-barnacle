1f24b29e4e7a6b563eb1747ecaf8676e2aedc580
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-01 10:03:01 +0900

    Update map.ts

--- src/functions/map.ts
+++ src/functions/map.ts
@@ -1,15 +1,9 @@
 export function map<T, U, TReturn, TNext>(iterator: Iterator<T, TReturn, TNext>, mapFunction: (value: T) => U): Generator<U, TReturn, TNext>;
 export function map<T, U, TReturn, UReturn, TNext>(iterator: Iterator<T, TReturn, TNext>, mapFunction: (value: T) => U, mapReturnFunction: (value: TReturn) => UReturn): Generator<U, UReturn, TNext>;
 export function map<T, U, TReturn, UReturn, TNext, UNext>(iterator: Iterator<T, TReturn, TNext>, mapFunction: (value: T) => U, mapReturnFunction: (value: TReturn) => UReturn, mapNextFunction: (value: UNext) => TNext): Generator<U, UReturn, UNext>;
-export function* map<T, U, TReturn, UReturn, TNext, UNext>(
-  iterator: Iterator<T, TReturn, TNext | UNext>,
-  mapFunction: (value: T) => U,
-  mapReturnFunction?: (value: TReturn) => UReturn,
-  mapNextFunction?: (value: TNext) => UNext
-): Generator<U, TReturn | UReturn, TNext> {
-  const iterator = from(iterable);
-
+export function* map<T, U, TReturn, UReturn, TNext, UNext>(iterator: Iterator<T, TReturn, TNext | UNext>, mapFunction: (value: T) => U, mapReturnFunction?: (value: TReturn) => UReturn, mapNextFunction?: (value: TNext) => UNext): Generator<U, TReturn | UReturn, TNext> {
   let cur = iterator.next();
+
   while (!cur.done) {
     try {
       const next = yield mapFunction(cur.value);
