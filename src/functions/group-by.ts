5c68ef6232f8aeb1a5106b550f48895740395943
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-02 15:43:31 +0900

    Create group-by.ts

---
+++ src/functions/group-by.ts
@@ -0,0 +1,34 @@
+export function* groupBy<T, TReturn, TNext>(iterator: Iterator<T, TReturn, TNext>, groupByFunction: (previous: T, current: T) => boolean): Generator<T[], TReturn, TNext> {
+  let prev = iterator.next();
+  if (prev.done) {
+    // iterator has no value
+    return prev.value;
+  }
+
+  let cur = iterator.next();
+
+  let valueCollector = [prev.value];
+  while (!cur.done && groupByFunction(prev.value, cur.value)) {
+    valueCollector.push(cur.value);
+    prev = cur;
+    cur = iterator.next();
+  }
+
+  while (!cur.done) {
+    try {
+      const next = yield valueCollector;
+
+      valueCollector = [];
+      while (!cur.done && groupByFunction(prev.value, cur.value)) {
+        valueCollector.push(cur.value);
+        prev = cur;
+        cur = iterator.next(next);
+      }
+    } catch (error) {
+      iterator.throw?.(error);
+    }
+  }
+
+  yield valueCollector;
+  return cur.value;
+}
