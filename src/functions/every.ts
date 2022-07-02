50fddb3b8590c4b0719729b40395165590bce162
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-03 01:10:51 +0900

    Create every.ts

---
+++ src/functions/every.ts
@@ -0,0 +1,12 @@
+export function every<T>(iterator: Iterator<T>, everyFunction: (value: T) => boolean): boolean {
+  let cur = iterator.next();
+
+  while (!cur.done) {
+    if (!everyFunction(cur.value)) {
+      return false;
+    }
+    cur = iterator.next();
+  }
+
+  return true;
+}
