66d454d7ca7478c7154829f331e415434901c426
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-03 00:58:22 +0900

    Create chunks.ts

---
+++ src/functions/chunks.ts
@@ -0,0 +1,20 @@
+export function* chunks<T, TReturn, TNext, N extends number>(iterator: Iterator<T, TReturn, TNext>, size: N): Generator<TupleOrLess<T, N>, TReturn, TNext> {
+  let cur = iterator.next();
+  let next: TNext;
+
+  while (!cur.done) {
+    const valueCollector = [];
+
+    for (let i = 0; i < size && !cur.done; i++) {
+      valueCollector.push(cur.value);
+      cur = iterator.next(next);
+    }
+    try {
+      next = yield valueCollector;
+    } catch (error) {
+      iterator.throw?.(error);
+    }
+  }
+
+  return cur.value;
+}
