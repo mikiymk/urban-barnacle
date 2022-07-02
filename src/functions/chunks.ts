8de03752fa464040738d02334e382a8d9ff30d4a
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-03 01:00:07 +0900

    Update chunks.ts

--- src/functions/chunks.ts
+++ src/functions/chunks.ts
@@ -1,3 +1,8 @@
+type TupleOrLess<I, L extends number, T extends I[] = []>
+  = L extends 0 ? []
+    : T["length"] extends L ? T
+    : T | Tuple<I, L, [...T, I]>;
+
 export function* chunks<T, TReturn, TNext, N extends number>(iterator: Iterator<T, TReturn, TNext>, size: N): Generator<TupleOrLess<T, N>, TReturn, TNext> {
   let cur = iterator.next();
   let next: TNext;
