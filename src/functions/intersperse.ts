fce4acf571a4c92be9634fc0e8f657accbe91cc6
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-03 00:17:13 +0900

    Update intersperse.ts

--- src/functions/intersperse.ts
+++ src/functions/intersperse.ts
@@ -1,10 +1,19 @@
 export function* intersperse<T, TNext, TReturn>(iterator: Iterator<T, TNext, TReturn>, separator: T): Generator<T, TNext, TReturn> {
   let cur = iterator.next();
 
+  if (!cur.done) {
+    try {
+      const next = yield cur.value;
+      cur = iterator.next(next);
+    } catch (error) {
+      iterator.throw?.(error);
+    }
+  }
+
   while (!cur.done) {
     try {
-      yield cur.value;
-      const next = yield separator;
+      yield separator;
+      const next = yield cur.value;
       cur = iterator.next(next);
     } catch (error) {
       iterator.throw?.(error);
