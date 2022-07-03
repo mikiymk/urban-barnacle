ec35cea9c501f8106c15112b123196f4612278f9
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-03 12:41:45 +0900

    Update count-up.test.ts

--- src/functions/count-up.test.ts
+++ src/functions/count-up.test.ts
@@ -3,18 +3,20 @@ import { countUp } from "./count-up";
 
 test("count up", () => {
   let it = countUp();
-  expect(it.next().value).toBe(0);
-  expect(it.next().value).toBe(1);
-  expect(it.next().value).toBe(2);
-  expect(it.next().value).toBe(3);
-  expect(it.next().value).toBe(4);
+
+  expect(it.next()).toEqual({ done: false, value: 0 });
+  expect(it.next()).toEqual({ done: false, value: 1 });
+  expect(it.next()).toEqual({ done: false, value: 2 });
+  expect(it.next()).toEqual({ done: false, value: 3 });
+  expect(it.next()).toEqual({ done: false, value: 4 });
 });
 
 test("count up start", () => {
   let it = countUp(5);
-  expect(it.next().value).toBe(5);
-  expect(it.next().value).toBe(6);
-  expect(it.next().value).toBe(7);
-  expect(it.next().value).toBe(8);
-  expect(it.next().value).toBe(9);
+
+  expect(it.next()).toEqual({ done: false, value: 5 });
+  expect(it.next()).toEqual({ done: false, value: 6 });
+  expect(it.next()).toEqual({ done: false, value: 7 });
+  expect(it.next()).toEqual({ done: false, value: 8 });
+  expect(it.next()).toEqual({ done: false, value: 9 });
 });
