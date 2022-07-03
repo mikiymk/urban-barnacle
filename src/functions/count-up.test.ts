3cf5b3eade2e1ce2901e5cc5bb3ca4ef80710fca
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-03 12:17:05 +0900

    Update count-up.test.ts

--- src/functions/count-up.test.ts
+++ src/functions/count-up.test.ts
@@ -3,18 +3,18 @@ import { countUp } from "./count-up";
 
 test("count up", () => {
   let it = countUp();
-  expect(it.next().value).toEqual(0);
-  expect(it.next().value).toEqual(1);
-  expect(it.next().value).toEqual(2);
-  expect(it.next().value).toEqual(3);
-  expect(it.next().value).toEqual(4);
+  expect(it.next().value).toBe(0);
+  expect(it.next().value).toBe(1);
+  expect(it.next().value).toBe(2);
+  expect(it.next().value).toBe(3);
+  expect(it.next().value).toBe(4);
 });
 
 test("count up start", () => {
   let it = countUp(5);
-  expect(it.next().value).toEqual(5);
-  expect(it.next().value).toEqual(6);
-  expect(it.next().value).toEqual(7);
-  expect(it.next().value).toEqual(8);
-  expect(it.next().value).toEqual(9);
+  expect(it.next().value).toBe(5);
+  expect(it.next().value).toBe(6);
+  expect(it.next().value).toBe(7);
+  expect(it.next().value).toBe(8);
+  expect(it.next().value).toBe(9);
 });
