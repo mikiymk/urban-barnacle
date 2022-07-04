1088e671399896d115744d05cfa2ec83f65693f0
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-04 14:12:18 +0900

    Update from.ts

--- src/functions/from.ts
+++ src/functions/from.ts
@@ -1,8 +1,5 @@
-export function* fromArray<T>(array: Array<T> | ArrayLike<T>): Iterator<T, any, unknown> {
-  for (let i = 0; i < array.length; i++) {
-    // lazy call
-    yield array[i];
-  }
+export function fromArray<T>(array: Array<T> | ArrayLike<T>): Iterator<T, any, unknown> {
+  return Array.prototype[Symbol.iterator].call(array);
 }
 
 export function fromIterable<T>(iterable: Iterable<T>): Iterator<T, any, unknown> {
@@ -10,7 +7,5 @@ export function fromIterable<T>(iterable: Iterable<T>): Iterator<T, any, unknown
 }
 
 export function from<T>(iterable: Array<T> | ArrayLike<T> | Iterable<T>): Iterator<T, any, unknown> {
-  return "length" in iterable && !Array.isArray(iterable)
-    ? fromArray(iterable)
-    : fromIterable(iterable);
+  return "length" in iterable ? fromArray(iterable) : fromIterable(iterable);
 }
