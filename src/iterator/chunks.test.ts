a6296166bd4dec4099a6f1946e10efb724078b52
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-06 17:25:03 +0900

    Update chunks.test.ts

--- src/iterator/chunks.test.ts
+++ src/iterator/chunks.test.ts
@@ -3,8 +3,8 @@ import { chunks } from "./chunks";
 import { from } from "./from";
 
 test("chunks 2", () => {
-  let it = from([1, 2, 3, 4, 5]);
-  it = chunks(it, 2);
+  let iterator = from([1, 2, 3, 4, 5]);
+  let it = chunks(it, 2);
 
   expect(it.next()).toEqual({ done: false, value: [1, 2] });
   expect(it.next()).toEqual({ done: false, value: [3, 4] });
@@ -12,21 +12,20 @@ test("chunks 2", () => {
   expect(it.next()).toEqual({ done: true, value: undefined });
 });
 
-const iterator = {
-  *[Symbol.iterator]() {
-    expect(yield 1).toBe(undefined);
-    expect(yield 2).toBe("1");
-    expect(yield 3).toBe(undefined);
-    expect(yield 4).toBe("3");
-    expect(yield 5).toBe("5");
+test("transmit next value", () => {
+  expect.assertions(9);
+  let iterator = {
+    *[Symbol.iterator]() {
+      expect(yield 1).toBe(undefined);
+      expect(yield 2).toBe("1");
+      expect(yield 3).toBe(undefined);
+      expect(yield 4).toBe("3");
+      expect(yield 5).toBe("5");
 
-    return 42;
-  }
-};
-
-test("count up iterator", () => {
-  let it = from([1, 2, 3, 4, 5]);
-  it = chunks(iterator, 2);
+      return 42;
+    }
+  };
+  let it = chunks(iterator, 2);
 
   expect(it.next("1")).toEqual({ done: false, value: [1, 2] });
   expect(it.next("3")).toEqual({ done: false, value: [3, 4] });
