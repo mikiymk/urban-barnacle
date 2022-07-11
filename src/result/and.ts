20b2d130bdd953ddfadf9ed37d103cba8feba22a
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-12 00:57:02 +0900

    Update and.ts

--- src/result/and.ts
+++ src/result/and.ts
@@ -1,3 +1,6 @@
+import { Result } from "./result";
+import { isFailure } from "./is-failure";
+
 export const and = <S extends {}, F extends Error>(result: Result<S, F>, andFunction: (value: S) => Result<S, F>): Result<S, F> => {
   if (isFailure(result)) {
     return result;
