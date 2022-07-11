a14ac9c65811a9e24d66ae70060dffe4b8427745
ymk <65121726+mikiymk@users.noreply.github.com>
2022-07-12 00:58:44 +0900

    Update or.ts

--- src/result/or.ts
+++ src/result/or.ts
@@ -1,3 +1,6 @@
+import { Result } from "./result";
+import { isSuccess } from "./is-success";
+
 export const or = <S extends {}, F extends Error>(result: Result<S, F>, orFunction: (error: F) => Result<S, F>): Result<S, F> => {
   if (isSuccess(result)) {
     return result;
