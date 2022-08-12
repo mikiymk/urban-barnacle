0c0b38bb2c244be1d1b1330c9149c48f9c8cd7c4
ymk <65121726+mikiymk@users.noreply.github.com>
2022-08-12 19:21:20 +0900

    Update result.ts

--- src/result.ts
+++ src/result.ts
@@ -1,7 +1,7 @@
-export type { Result, ResultSuccess, ResultFailure, SuccessValue } from "./result/result";
-export { isSuccess, isFailure } from "./result/guard";
-export { success, failure, result } from "./result/wrap";
 export { and } from "./result/and";
+export { isSuccess, isFailure } from "./result/guard";
 export { or } from "./result/or";
 export { getValue, getError } from "./result/unwrap";
+export { success, failure, result } from "./result/wrap";
 
+export type { Result, ResultSuccess, ResultFailure, SuccessValue } from "./result/result";
