e3c3b26be81d39b32a4153031d9fdfef71c2f84b
ymk <65121726+mikiymk@users.noreply.github.com>
2022-08-12 16:16:01 +0900

    Create result.ts

---
+++ src/result.ts
@@ -0,0 +1,7 @@
+export { success, failure, type Result, type ResultSuccess, type ResultFailure, type SuccessValue } from "./result/result";
+export { isSuccess, isFailure } from "./result/guard";
+export { wrap } from "./result/wrap";
+export { and } from "./result/and";
+export { or } from "./result/or";
+export { getValue, getError } from "./result/unwrap";
+
