4439a56ca69e725c9f7f589a3f5b272dc014e131
ymk <65121726+mikiymk@users.noreply.github.com>
2022-08-12 19:04:30 +0900

    add generate result test

--- src/result/wrap.test.ts
+++ src/result/wrap.test.ts
@@ -1,17 +1,30 @@
 import { describe, expect, test } from "vitest";
 
-import { success, failure } from "./result";
-import { wrap } from "./wrap";
+import { success, failure, result } from "./wrap";
+
+describe("generate result", () => {
+  test("success", () => {
+    const actual = success(42),
+      expected = [42] as const;
+    expect(actual).toEqual(expected);
+  });
+
+  test("failure", () => {
+    const actual = failure(new Error("foo")),
+      expected = [undefined, new Error("foo")] as const;
+    expect(actual).toEqual(expected);
+  });
+});
 
 describe("wrap result", () => {
   test("wrap success", () => {
-    const actual = wrap(() => 42),
+    const actual = result(() => 42),
       expected = success(42);
     expect(actual).toEqual(expected);
   });
 
   test("wrap failure", () => {
-    const actual = wrap(() => {
+    const actual = result(() => {
         throw new Error("foo");
       }),
       expected = failure(new Error("foo"));
@@ -19,7 +32,7 @@ describe("wrap result", () => {
   });
 
   test("wrap non-error failure", () => {
-    const actual = wrap(() => {
+    const actual = result(() => {
         // eslint-disable-next-line @typescript-eslint/no-throw-literal
         throw "foo";
       }),
